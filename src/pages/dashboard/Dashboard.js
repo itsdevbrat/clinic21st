0122df6ccfb9230f7c47a9b92fb4dffd83d25a3c
itsdevbrat <debabrata.d@somaiya.edu>
2021-09-09 08:45:24 +0530

    wip

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Option from '../../components/option/Option'
 import UserForm from '../../components/userform/UserForm'
 import VerticalForm from '../../components/verticalform/VerticalForm'
 import UsersTable from '../../components/userstable/UsersTable'
+import Button from '@material-ui/core/Button';
+import useLocalStorage from '../../customHooks/useLocalStorage'
 import './Dashboard.css';
 
 const Dashboard = props => {
@@ -11,6 +13,7 @@ const Dashboard = props => {
     const [showUserForm, setShowUserForm] = useState(false)
     const [showVerticalForm, setShowVerticalForm] = useState(false)
     const [showUsersTable, setShowUsersTable] = useState(false)
+    const [jwt, setJwt] = useLocalStorage('auth-token', '')
 
     let toggleUserForm = () => {
         console.log('toggling form')
@@ -27,11 +30,24 @@ const Dashboard = props => {
         showUsersTable ? setShowUsersTable(false) : setShowUsersTable(true)
     }
 
+    let logout = () => {
+        setJwt('')
+        window.location.href = 'http://localhost:3000/login'
+    }
+
+    // useEffect(()=>{
+    //     if(!jwt)
+    //         logout()
+    // }, [])
+
 
 
     return (
         <div>
-            <h1 className="title">Dashboard</h1>
+            <div className="title">
+                <h1>Dashboard</h1>
+            {!jwt && <Button color='secondary' onClick={logout}>Sign out</Button>}
+            </div>
             <Option title='Create User' onClick={toggleUserForm} />
             {showUserForm && <UserForm toggleForm = {toggleUserForm} />}
             <Option title='Add a vertical' onClick={toggleVerticalForm} />
