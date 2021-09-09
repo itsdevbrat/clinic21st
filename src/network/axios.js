da8c97bad13f6a44ef96e21919cc8bf967dfaa7d
Devbrat Dash <48611876+itsdevbrat@users.noreply.github.com>
2021-09-09 23:12:06 +0530

    Update axios.js

--- src/network/axios.js
+++ src/network/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "https://clinic21st.heroku.com/",
   headers: {
     "Content-type": "application/json"
   }
