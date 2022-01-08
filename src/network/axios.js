20ed36cf1db980da7555c3eff410a2a16f0012cb
Devbrat Dash <48611876+itsdevbrat@users.noreply.github.com>
2022-01-08 18:10:55 +0530

    Update axios.js

--- src/network/axios.js
+++ src/network/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: window.location.href.match('.*localhost.*') ? 'http://localhost:8000' : 'https://31.220.109.99/',
+  baseURL: window.location.href.match('.*localhost.*') ? 'http://localhost:8000' : 'https://api.clinic21st.in/',
   headers: {
     "Content-type": "application/json"
   }
