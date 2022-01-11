a77535acbf14d080e0ed6ae6b006eaac3c1754d2
Devbrat Dash <48611876+itsdevbrat@users.noreply.github.com>
2022-01-11 20:09:17 +0530

    Update UserForm.js

--- src/components/userform/UserForm.js
+++ src/components/userform/UserForm.js
@@ -67,7 +67,7 @@ const UserForm = ({ toggleForm }) => {
 
     const saveUser = (values, verticalsSelected) => {
         values.verticals = verticalsSelected
-        values.birthDate = values.birthDate.includes(' 00:00:00')  ? values.birthDate : values.birthDate + ' 00:00:00'
+        values.birthDate = values.birthDate.includes('T00:00:00')  ? values.birthDate : values.birthDate + 'T00:00:00'
         console.log(values)
         console.log(jwt)
         setLoader(true)
